6fea6376b36ba8c56528c3538fa1f6c142c9d5b5
Santyid <santiago-2108@hotmail.com>
2018-04-27 15:10:30 -0500

    aja

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
+	constructor(public navCtrl: NavController, public cameraPlugin: Camera) {}
 
-  constructor(public navCtrl: NavController) {
+	takePicture(): void {
+		const options: CameraOptions = {
+			quality: 100,
+			destinationType: this.cameraPlugin.DestinationType.DATA_URL,
+			encodingType: this.cameraPlugin.EncodingType.JPEG,
+			mediaType: this.cameraPlugin.MediaType.PICTURE
+		};
 
-  }
-
+		this.cameraPlugin.getPicture(options).then(
+			(imageData) => {
+				// imageData is either ax base64 encoded string or a file URI
+				// If it's base64:
+				let base64Image = 'data:image/jpeg;base64,' + imageData;
+			},
+			(err) => {
+				// Handle error
+			}
+		);
+	}
 }
